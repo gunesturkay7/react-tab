1717bedbad92967b627049e7e38cdacd0f14f870
gunesturkay7 <rgunesturkay@gmail.com>
2022-05-18 15:48:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# Simple React JS Project
+
+## What is the use of this Repo
+
+This Project is a Simple ReactJS Project which demonstrates the following
+
+1. Creating a Component in React
+2. Show Hide Component
+
+The project Template can be used to build bigger projects
+
+### Install Node JS
+
+Refer to https://nodejs.org/en/ to install nodejs
+
+### Install create-react-app
+
+Install create-react-app npm package globally. This will help to easily run the project and also build the source files easily. Use the following command to install create-react-app
+
+```bash
+npm install -g create-react-app
+```
+
+## Cloning and Running the Application in local
+
+Clone the project into local
+
+Install all the npm packages. Go into the project folder and type the following command to install all npm packages
+
+```bash
+npm install
+```
+
+In order to run the application Type the following command
+
+```bash
+npm start
+```
+
+The Application Runs on **localhost:3000**
+
+## Application design
+
+# Simple React JS Project
+
+## What is the use of this Repo
+
+This Project is a Simple ReactJS Project which demonstrates the following
+
+1. Creating a Component in React
+2. Show Hide Component
+
+The project Template can be used to build bigger projects
+
+### Install Node JS
+
+Refer to https://nodejs.org/en/ to install nodejs
+
+### Install create-react-app
+
+Install create-react-app npm package globally. This will help to easily run the project and also build the source files easily. Use the following command to install create-react-app
+
+```bash
+npm install -g create-react-app
+```
+
+## Cloning and Running the Application in local
+
+Clone the project into local
+
+Install all the npm packages. Go into the project folder and type the following command to install all npm packages
+
+```bash
+npm install
+```
+
+In order to run the application Type the following command
+
+```bash
+npm start
+```
+
+The Application Runs on **localhost:3000**
+
+## Application design
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tabs-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const getJobs = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setJobs(data);
+    } catch (err) {
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getJobs();
+  }, []);
+  if (loading) {
+    return (
+      <section className="setion loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                className={`job-btn ${index === value && "active-btn"}`}
+                key={index}
+                onClick={() => setValue(index)}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div className="job-desc" key={index}>
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
+}
+
+export default App;
